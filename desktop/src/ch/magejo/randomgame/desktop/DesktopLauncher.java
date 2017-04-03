6fe6fbf44ba96e8e74f75a0347f89aded836ccd1
Saturn91 <manuel.geissberger@gmail.com>
2017-04-03 15:13:30 +0200

    [EDIT] changed FPS to 60

--- desktop/src/ch/magejo/randomgame/desktop/DesktopLauncher.java
+++ desktop/src/ch/magejo/randomgame/desktop/DesktopLauncher.java
@@ -8,6 +8,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Random Game";
+		config.vSyncEnabled = false;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 15;
 		config.width = 1024;
 		config.height = 720;
 		config.fullscreen = true;
