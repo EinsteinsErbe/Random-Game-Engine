07ee06db9e998823dc89f36cbaee46b335ba6c96
Manuel Jordan <manuel.jordan@ultraweb.ch>
2017-04-05 23:19:42 +0200

    [REFACTOR] cast cam pos to int

--- core/src/ch/magejo/randomgame/objects/TileSet.java
+++ core/src/ch/magejo/randomgame/objects/TileSet.java
@@ -9,30 +9,31 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ch.magejo.randomgame.utils.Log;
+import ch.magejo.randomgame.utils.math.Vector2i;
 
 public class TileSet {
 	private Texture tileSet;
 	private int textureWidth;
 	private int textureHeight;
-	private HashMap<Integer, Vector2> addressedTiles;
+	private HashMap<Integer, Vector2i> addressedTiles;
 	private TextureRegion actualRegion;
 
 	public TileSet(String tileset, int textureWidth, int textureHeigth) {
 		this.tileSet = new Texture(tileset);
 		this.textureWidth = textureWidth;
 		this.textureHeight = textureHeigth;
-		this.addressedTiles = new HashMap<Integer, Vector2>();
+		this.addressedTiles = new HashMap<Integer, Vector2i>();
 		this.actualRegion = new TextureRegion(tileSet);
 	}
 
 	public void addTile(int xPos, int yPos, int id){
-		addressedTiles.put(id, new Vector2(xPos, yPos));
+		addressedTiles.put(id, new Vector2i(xPos, yPos));
 	}
 
 	public void render(SpriteBatch batch, float x, float y, int addres){
 		actualRegion.setRegion(
-				(int) addressedTiles.get(addres).x*textureWidth,
-				(int) addressedTiles.get(addres).y*textureHeight,
+				addressedTiles.get(addres).x*textureWidth,
+				addressedTiles.get(addres).y*textureHeight,
 				textureWidth,
 				textureHeight
 				);
